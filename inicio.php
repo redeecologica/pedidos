c5cc01e010ad48e150eff728a896e5f33691abe3
Aline Almeida <alinesudoku@gmail.com>
2013-04-21 00:05:49 -0300

    notícia da página inicial agora vem do banco de dados

--- inicio.php
+++ inicio.php
@@ -5,19 +5,14 @@
 ?>
 <legend>Notícias da Rede</legend>
 
-
-<h4>Sistema de pedidos on-line</h4>
-
-Entra no ar o nosso sistema de pedidos on-line.<br>
-O primeiro pedido realizado pelo sistema online foi o de frescos com entrega no dia 06/abril.  <br>
-Foram registrados 103 pedidos! Foi um grande teste para o nosso sistema, que será gradativamente aperfeiçoado.<br><br>
-É importante que você atualize os seus dados (opção "Minha Conta" / "Meus Dados").<br>
-Caso precise alterar o núcleo ou status de sua associação, procure o <a href="ajuda.php">responsável pelo cadastro no seu núcleo</a> (somente ele poderá alterar este tipo de informação).
-
-<hr />
-
-
 <?php 
+
+	 $sql = "SELECT not_publicado FROM noticia ORDER BY dt_atualizacao DESC LIMIT 1 ";
+	 $res = executa_sql($sql);
+  	 if ($row = mysqli_fetch_array($res,MYSQLI_ASSOC)) 
+	 {	
+		  	echo( $row["not_publicado"]);	
+	 }
 
 	footer();
 ?>
