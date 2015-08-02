be8264ef1506316c44a7ba4e904f8931e455db66
Aline Ribeiro de Almeida <alinesudoku@gmail.com>
2015-08-02 13:27:31 -0300

    atualização bibliotecas bootstrap e jquery
    atualização bibliotecas bootstrap de 3.1.1 para 3.3.5 e jquery de 1.9.1
    para 1.11.3

--- header.inc.php
+++ header.inc.php
@@ -6,13 +6,19 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo(NOME_SISTEMA); ?></title>
-	<link href="css/bootstrap-3.1.1.min.css" rel="stylesheet" media="screen" />
-	<link href="css/bootstrap-theme.min.css" rel="stylesheet" media="screen" />
+	<link href="css/bootstrap-3.3.5.min.css" rel="stylesheet" media="screen" />
+	<link href="css/bootstrap-theme.min.css?ver=3.3.5" rel="stylesheet" media="screen" />
 	<link href="css/complemento.css?ver=1.2" rel="stylesheet" media="screen" /> 
 	<link href="css/datepicker.css" rel="stylesheet" media="screen">
     
-  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script src="js/bootstrap-3.1.1.min.js"></script>
+  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>
+		if (typeof jQuery === 'undefined') 
+		{
+	  		document.write(unescape('%3Cscript%20src%3D%22js/jquery-1.11.3.min.js%22%3E%3C/script%3E'));
+		}
+	</script>    
+	<script src="js/bootstrap-3.3.5.min.js"></script>
 	<script src="js/pedido.js?ver=1.7.8"></script>
 	<script src="js/bootstrap-datepicker.js" charset="UTF-8"></script>
     <script src="js/locales/bootstrap-datepicker.pt-BR.js" charset="UTF-8"></script>
