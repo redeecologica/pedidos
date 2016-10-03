ce0227da1741bcd0f553c6a6210ccbd8d81113a3
Aline Ribeiro de Almeida <alinesudoku@gmail.com>
2016-10-02 22:30:14 -0300

    melhoria relatorio produtos ao longo do tempo
    melhoria no relatório de pedido de produtos ao longo de um período,
    permitindo filtro por faixa de datas de entrega, tipo de chamada, nome
    do produtor e nome do produto

--- rel_produtos_ao_longo_do_ano.php
+++ rel_produtos_ao_longo_do_ano.php
@@ -11,20 +11,27 @@
  <div class="panel panel-default">
   <div class="panel-heading">
      <strong>Produtos Disponíveis ao Longo do Ano</strong>
-  </div>
+  </div> 
+ 
  <div class="panel-body">
- 
- 	<?php  		
-  		$ano = request_get("ano",date("Y")) ;
-		$prodt = request_get("prodt",-1) ;
+  <form class="form-inline" method="post" name="frm_filtro" id="frm_filtro">
+
+	<?php  
+  		
+		$cha_prodt = request_get("cha_prodt",-1);
+		$forn_prodt = request_get("forn_prodt",-1);	
+		$cha_dt_ini = request_get("cha_dt_ini",date("d/m/Y", strtotime("-3 months"))) ;
+		$cha_dt_fim = request_get("cha_dt_fim",date("d/m/Y")) ;		
+		$forn_nome = request_get("forn_nome","") ;
+		$prod_nome = request_get("prod_nome","") ;
+		$chamada_tipos = array();
+						
 	?>
-    
- <form class="form-inline" method="post" name="frm_filtro" id="frm_filtro">
- 
-     <fieldset>    
-      <div class="form-group">
-  				<label for="prodt">Tipo: </label>            
-                 <select name="prodt" id="prodt" class="form-control">
+     <fieldset>     
+     	<div class="form-group">
+  				<label for="cha_prodt">Tipo Chamada: </label>            
+                 <select name="cha_prodt" id="cha_prodt" class="form-control">
+                        <option value="-1" <?php echo(($cha_prodt==-1)?" selected" : ""); ?> >TODOS</option>
 						<?php
                             
                             $sql = "SELECT prodt_id, prodt_nome ";
@@ -36,47 +43,115 @@
                               while ($row = mysqli_fetch_array($res,MYSQLI_ASSOC)) 
                               {
                                  echo("<option value='" . $row['prodt_id'] . "'");
-                                 if($row['prodt_id']==$prodt) echo(" selected");
+                                 if($row['prodt_id']==$cha_prodt) echo(" selected");
+                                 echo (">" . $row['prodt_nome'] . "</option>");
+							  	 $chamada_tipos[$row['prodt_id']]=$row['prodt_nome'];
+                              }
+
+ 
+                            }
+							
+                        ?>  
+                 </select>    
+         </div>   
+
+
+        &nbsp;&nbsp
+        <div class="form-group">            
+  				
+                 <label class="control-label" for="cha_dt_ini">Data entrega min: </label>            
+                   <input type="text" name="cha_dt_ini" required="required" value="<?php echo($cha_dt_ini); ?>"  class="form-control"/>                    
+                 
+                 &nbsp;&nbsp
+                 <label class="control-label" for="cha_dt_fim">máx: </label>            
+                   <input type="text" name="cha_dt_fim" required="required" value="<?php echo($cha_dt_fim); ?>"  class="form-control"/>   
+         </div> 
+         
+      
+<p />      
+         
+     	<div class="form-group">
+  				<label for="forn_prodt">Tipo Produtor: </label>            
+                 <select name="forn_prodt" id="forn_prodt" class="form-control">
+                        <option value="-1" <?php echo(($forn_prodt==-1)?" selected" : ""); ?> >TODOS</option>
+						<?php
+                            
+                            $sql = "SELECT prodt_id, prodt_nome ";
+                            $sql.= "FROM produtotipos ";
+                            $sql.= "ORDER BY prodt_nome ";
+                            $res = executa_sql($sql);
+                            if($res)
+                            {
+                              while ($row = mysqli_fetch_array($res,MYSQLI_ASSOC)) 
+                              {
+                                 echo("<option value='" . $row['prodt_id'] . "'");
+                                 if($row['prodt_id']==$forn_prodt) echo(" selected");
                                  echo (">" . $row['prodt_nome'] . "</option>");
                               }
                             }
                         ?>  
                  </select>    
-        </div>      
-                 &nbsp;
-         <div class="form-group">            
-  				<label for="ano">Ano: </label>            
-                <input type="text" name="ano" id="ano" class="form-control" value="<?php echo($ano); ?>">
-         </div>&nbsp;&nbsp;
-           <div class="form-group">
-			<button class="btn btn-primary btn-enviando" data-loading-text="consultando..." type="submit" class="form-control"><i class="glyphicon glyphicon-search glyphicon-white"></i> consultar</button>
-            </div>
-                                           
+         </div>                
+    
+         
+         
+         &nbsp;
+        <div class="form-group">            
+               <label class="control-label" for="forn_nome">Produtor: </label>
+                   <input type="text" name="forn_nome" value="<?php echo($forn_nome); ?>" placeholder="ex: *brejal*"  class="form-control"/>
                     
-     </fieldset>
-</form>
+         </div> 
+         
+        &nbsp;
+        <div class="form-group">            
+               <label class="control-label" for="prod_nome">Produto: </label>
+                   <input type="text" name="prod_nome"  value="<?php echo($prod_nome); ?>" placeholder="ex: banana*" class="form-control"/>                    
+         </div> 
+            
+          &nbsp;&nbsp &nbsp;&nbsp
+         
+         <button type="submit" class="btn btn-default btn-enviando"  data-loading-text="aguarde..." >Consultar</button>
+                                                   
+                    
+      </fieldset>
+  </form>
  </div>
+ 
+ 
  
  </div>
  
 <?php
- if($prodt!=-1 )
- {
-	 $sql = "SELECT forn_nome_curto, prod_id, prod_nome, prod_unidade, FORMAT(prod_valor_compra, 2) prod_valor_compra,";
-	 $sql.= "chaprod_disponibilidade, cha_id, FORMAT(SUM(pedprod_quantidade), 1) total_pedido, cha_dt_entrega "; 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+ {	 
+	 $sql = "SELECT forn_nome_curto, prod_id, prod_nome, prod_unidade, prod_valor_compra, ";
+	 $sql.= " MAX(chaprod_disponibilidade) chaprod_disponibilidade, FORMAT(SUM(pedprod_quantidade), 1) total_pedido, cha_dt_entrega, MIN(cha_prodt) cha_prodt "; 
 	 $sql.= " FROM produtos ";
-	 $sql.= " LEFT JOIN fornecedores ON prod_forn = forn_id";
-	 $sql.= " LEFT JOIN chamadas ON cha_prodt = prod_prodt AND year(cha_dt_entrega)= " . prep_para_bd($ano);
+	 $sql.= " LEFT JOIN fornecedores ON prod_forn = forn_id ";	 
+	 $sql.= " CROSS JOIN chamadas "; 
 	 $sql.= " LEFT JOIN pedidoprodutos ON pedprod_prod=prod_id AND pedprod_ped IN ";
 	 $sql.= "  (SELECT ped_id FROM pedidos where ped_cha = cha_id AND ped_fechado = '1' )";
 	 $sql.= " LEFT JOIN chamadaprodutos ON chaprod_cha = cha_id AND chaprod_prod = prod_id ";
-	 $sql.= " WHERE  ";
-	 $sql.= " prod_prodt = " .  prep_para_bd($prodt) . " AND ";
-	 $sql.= " prod_ini_validade <= now() AND ";
-	 $sql.= " prod_fim_validade >= now() ";	
-	 $sql.= " GROUP BY forn_nome_curto , prod_nome, prod_unidade, cha_dt_entrega ";
-	 $sql.= " ORDER BY forn_nome_curto , prod_nome , prod_unidade, cha_dt_entrega ";
-	 $res = executa_sql($sql);
+	 $sql.= " WHERE  ";	 
+	 if($cha_prodt!=-1) $sql.= " cha_prodt = " .  prep_para_bd($cha_prodt) . " AND ";	
+	 if($forn_prodt!=-1) $sql.= " forn_prodt = " .  prep_para_bd($forn_prodt) . " AND ";		 
+	 if($forn_nome!="")
+	 {
+		  $sql.= " ( forn_nome_curto LIKE " .  prep_para_bd(str_replace('*','%',$forn_nome)) . " OR ";	 
+		  $sql.= " forn_nome_completo LIKE " .  prep_para_bd(str_replace('*','%',$forn_nome)) . " ) ";	 		
+		  $sql.= " AND " ;
+	 }	 
+	 if($prod_nome!="") $sql.= " prod_nome LIKE " .  prep_para_bd(str_replace('*','%',$prod_nome)) . " AND " ;	 	 
+	 
+	 $sql.= " DATE(cha_dt_entrega) >= " . prep_para_bd(formata_data_para_mysql($cha_dt_ini)) . " AND ";
+	 $sql.= " DATE(cha_dt_entrega) <= " . prep_para_bd(formata_data_para_mysql($cha_dt_fim)) . " AND ";	 	 	 	 
+	 $sql.= " DATE(prod_ini_validade) <= " . prep_para_bd(formata_data_para_mysql($cha_dt_fim)) . " AND ";
+	 $sql.= " DATE(prod_fim_validade) >= " . prep_para_bd(formata_data_para_mysql($cha_dt_fim)) . "   ";	
+	 $sql.= " GROUP BY cha_prodt, forn_nome_curto , prod_id, cha_dt_entrega ";
+	 $sql.= " ORDER BY cha_prodt, forn_nome_curto , prod_nome , prod_unidade, cha_dt_entrega ";
+	 $res = executa_sql($sql);	 
+
  }
 
 ?> 
@@ -87,34 +162,65 @@
 if($res) 
 { 
 	?>
-    <table class="table table-bordered table-condensed">
+    
+    <p /> 
+    <input class="btn btn-info" type="button" value="selecionar tabela para copiar" onclick="selectElementContents( document.getElementById('selectable') );">
+           
+    <p />
+    
+    <table id="selectable"  class="table table-bordered table-condensed">
+     
     <thead>
       <tr>
-        <th rowspan="2">Produtor</th>
-        <th rowspan="2">Produto</th>
-        <th rowspan="2">Unidade</th>        
-        <th rowspan="2">Valor</th>
+        <th rowspan="3">Chamada</th>
+        <th rowspan="3">Produtor</th>
+        <th rowspan="3">Produto</th>
+        <th rowspan="3">Unidade</th>        
+        <th rowspan="3">Valor compra em <?php echo($cha_dt_fim);?> </th>
+   
         
         <?php
-        
-		 $sql = "SELECT MONTH(cha_dt_entrega) AS mes, COUNT(MONTH(cha_dt_entrega)) total ";
-		 $sql.= " FROM chamadas ";
-		 $sql.= " WHERE YEAR(cha_dt_entrega)= " . prep_para_bd($ano) . " AND cha_prodt = " . prep_para_bd($prodt);
-		 $sql.= " GROUP BY MONTH(cha_dt_entrega) ORDER BY cha_dt_entrega ";
+         
+		 
+		 $filtro_chamadas= " cha_prodt = " .  prep_para_bd($cha_prodt) . " AND ";	 
+	 	 $filtro_chamadas.= " DATE(cha_dt_entrega) >= " . prep_para_bd(formata_data_para_mysql($cha_dt_ini)) . " AND ";
+	 	 $filtro_chamadas.= " DATE(cha_dt_entrega) <= " . prep_para_bd(formata_data_para_mysql($cha_dt_fim));	 	 	 	 
+		 
+
+		 
+		 $sql = "SELECT YEAR(cha_dt_entrega) AS ano, COUNT(YEAR(cha_dt_entrega)) total ";
+		 $sql.= " FROM chamadas WHERE ";
+		 $sql.= $filtro_chamadas;		 
+		 $sql.= " GROUP BY YEAR(cha_dt_entrega) ORDER BY cha_dt_entrega ";
 		 $res2 = executa_sql($sql);	 
-	 	$total_entregas = 0;
+	 	 $total_entregas = 0;
+	     while ($entregas_ano = mysqli_fetch_array($res2,MYSQLI_ASSOC)) 
+		 {
+		 	$total_entregas+= $entregas_ano["total"];
+			 echo("<th colspan='". $entregas_ano["total"] . "'>" . $entregas_ano["ano"] . "</th>");
+		 }	
+		 echo("</tr>");	 
+		 
+		 
+		 
+		 $sql = "SELECT MONTH(cha_dt_entrega) AS mes, COUNT(MONTH(cha_dt_entrega)) total ";
+		 $sql.= " FROM chamadas WHERE ";
+		 $sql.= $filtro_chamadas;		 
+		 $sql.= " GROUP BY YEAR(cha_dt_entrega), MONTH(cha_dt_entrega) ORDER BY cha_dt_entrega ";
+		 $res2 = executa_sql($sql);	 
+	 	 $total_entregas = 0;
 	     while ($entregas_mes = mysqli_fetch_array($res2,MYSQLI_ASSOC)) 
 		 {
 		 	$total_entregas+= $entregas_mes["total"];
 			 echo("<th colspan='". $entregas_mes["total"] . "'>" . $meses[$entregas_mes["mes"]] . "</th>");
-		 }	
+		 }
+		 	
 		 echo("</tr>");
 		 
 		 echo("<tr>");
 		 $sql = "SELECT DAY(cha_dt_entrega) AS dia ";
-		 $sql.= " FROM chamadas ";
-		 $sql.= " WHERE YEAR(cha_dt_entrega)= " . prep_para_bd($ano) . " AND cha_prodt = " . prep_para_bd($prodt);
-		 $sql.= " ORDER BY cha_dt_entrega";
+		 $sql.= " FROM chamadas WHERE ";
+ 		 $sql.= $filtro_chamadas;
 		 $res2 = executa_sql($sql);
 	     while ($entregas_dia = mysqli_fetch_array($res2,MYSQLI_ASSOC)) 
 		 {
@@ -132,17 +238,18 @@ if($res)
 	
 	if($total_entregas==0)
 	{
-		echo("<tr><td colspan='4'>Nenhuma chamada para o período informado</td></tr>");
+		echo("<tr><td colspan='5'>Nenhuma chamada para o período informado</td></tr>");
 	}
 	else
 	{
  	while($produto = mysqli_fetch_array($res,MYSQLI_ASSOC))
 	{
 		echo("<tr>");
+		echo("<td>" . $chamada_tipos[$produto["cha_prodt"]] . "</td>");
 		echo("<td>" . $produto["forn_nome_curto"] . "</td>");
 		echo("<td>" . $produto["prod_nome"] . "</td>");
 		echo("<td>" . $produto["prod_unidade"] . "</td>");
-		echo("<td>" . formata_moeda($produto["prod_valor_compra"]) . "</td>");		
+		echo("<td>" . formata_moeda($produto["prod_valor_compra"]) . "</td>");	
 		for($i=1; $i <= $total_entregas; $i++)
 		{
 			if($i<>1) $produto = mysqli_fetch_array($res,MYSQLI_ASSOC);
