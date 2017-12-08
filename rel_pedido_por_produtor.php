3affa64786507a582a8ae8309f3d6d8dd0efd45c
Aline Ribeiro de Almeida <alinesudoku@gmail.com>
2017-12-08 06:00:54 -0200

    ajuste ordenação relatorio pedido ao produtor

--- rel_pedido_por_produtor.php
+++ rel_pedido_por_produtor.php
@@ -55,7 +55,7 @@ $sql.="AND ped_fechado = '1' ";
 $sql.="AND chaprod_disponibilidade <> '0' ";
 $sql.="AND prod_ini_validade<=cha_dt_entrega AND prod_fim_validade>=cha_dt_entrega  ";
 $sql.="GROUP BY  forn_id,prod_id, prod_unidade, nuc_id ";
-$sql.="ORDER BY forn_nome_curto,prod_nome, nuc_nome_curto";
+$sql.="ORDER BY forn_nome_curto,prod_nome, prod_unidade, nuc_nome_curto";
 
 
 $res = executa_sql($sql);
